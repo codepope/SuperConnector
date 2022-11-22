899a719e60ef8b143bc5de3477040c6163a2c9db
Dj Walker-Morgan <codepope@gmail.com>
2022-11-22 20:18:46 +0000

    Initial Commit

---
+++ SuperConnector/SuperConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperConnectorApp.swift
+//  SuperConnector
+//
+//  Created by Dj Walker-Morgan on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
